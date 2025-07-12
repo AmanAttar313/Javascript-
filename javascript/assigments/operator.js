7847eea776f7732fb6cce2ee0f5f6483d467b637
Aman Attar <amanatar311@gmail.com>
2025-07-12 22:29:08 +0530

    Add files via upload

---
+++ javascript/assigments/operator.js
@@ -0,0 +1,20 @@
+// Assignment 1: Basic Calculator
+let num1 = 10;
+let num2 = 5;
+
+console.log("Addition: " + (num1 + num2));
+console.log("Subtraction: " + (num1 - num2));
+console.log("Multiplication: " + (num1 * num2));
+console.log("Division: " + (num1 / num2));
+console.log("Modulus: " + (num1 % num2));
+
+// Assignment 2: Template Literal
+let name = "Aman";
+let city = "Baramati";
+
+console.log(`Hello, my name is ${name} and I live in ${city}.`);
+
+// Assignment 3: Mixed Type Expressions
+console.log("Number + String: " + (5 + "hello"));     
+console.log("Boolean + Number: " + (true + 3));         
+console.log("String + Boolean: " + ("yes" + false));    
